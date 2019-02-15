e8202463c79e46c89fd3c658a21fb6ceef0e181b
ZoeBraddock <zoe.braddock@vista.co>
2019-02-15 14:50:40 +1300

    Initial Commit for Name of App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+
+var business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business]
 
 class App extends Component {
+constructor(props) {
+  super(props);
+  this.state= {
+    'businesses': []
+  }
+  this.searchYelp = this.searchYelp.bind(this);
+}
+
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching with ${term}, ${location}, ${sortBy}`);
+}
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>ravenous</h1>
+        <SearchBar searchYelp ={this.searchYelp}/>
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
